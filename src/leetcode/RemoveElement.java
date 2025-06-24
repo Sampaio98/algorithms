dad8a76035560717754b3234cfa75ecf9248a06d
allisson.sampaio <allisson.sampaio@agi.com.br>
2025-06-24 00:21:04 -0300

    ajuste codigo remove element

--- src/leetcode/RemoveElement.java
+++ src/leetcode/RemoveElement.java
@@ -3,13 +3,22 @@ package leetcode;
 public class RemoveElement {
 
     public static void main(String[] args) {
-        var test1 = removeElement(new int[]{3, 2, 2, 3}, 3);
-        var test2 = removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2);
-        System.out.println(test1); // Expected: 2
-        System.out.println(test2); // Expected: 5
+//        var test1 = removeElement(new int[]{3, 2, 2, 3}, 3);
+//        var test2 = removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2);
+        var test3 = removeElement(new int[]{1}, 1);
+//        var test4 = removeElement(new int[]{2}, 3);
+//        var test5 = removeElement(new int[]{}, 0);
+//        System.out.println(test1); // Expected: 2
+//        System.out.println(test2); // Expected: 5
+        System.out.println(test3); // Expected: 1
+//        System.out.println(test4); // Expected: 2
+//        System.out.println(test5); // Expected: ?
     }
 
     public static int removeElement(int[] nums, int val) {
+        if (nums.length < 1 || nums.length == 1 && nums[0] == val) {
+            return 0;
+        }
         int aux = 0;
         int lastIndex = nums.length - 1;
         while (aux < lastIndex) {
@@ -27,7 +36,7 @@ public class RemoveElement {
                 aux++;
             }
         }
-        return aux + 1;
+        return aux + (nums[aux] == val ? 0 : 1);
     }
 
 }
