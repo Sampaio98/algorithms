ef6afc370225602a265525100207a77ac1572f6f
allisson.sampaio <allisson.sampaio@agi.com.br>
2025-06-24 00:01:51 -0300

    remove element algorithm

---
+++ src/leetcode/RemoveElement.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+public class RemoveElement {
+
+    public static void main(String[] args) {
+        var test1 = removeElement(new int[]{3, 2, 2, 3}, 3);
+        var test2 = removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2);
+        System.out.println(test1); // Expected: 2
+        System.out.println(test2); // Expected: 5
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int aux = 0;
+        int lastIndex = nums.length - 1;
+        while (aux < lastIndex) {
+            if (nums[aux] == val) {
+                if (nums[lastIndex] == val) {
+                    lastIndex--;
+                } else {
+                    int num = nums[aux];
+                    nums[aux] = nums[lastIndex];
+                    nums[lastIndex] = num;
+                    aux++;
+                    lastIndex--;
+                }
+            } else {
+                aux++;
+            }
+        }
+        return aux + 1;
+    }
+
+}
