ecda15092f2134358574bf884ecfb1b1514a8bd1
Allisson <allissonsampaio98@gmail.com>
2025-09-11 21:17:10 -0300

    leetcode 11/09

--- src/leetcode/Palindrome.java
+++ src/leetcode/Palindrome.java
@@ -8,26 +8,16 @@ public class Palindrome {
     }
 
     private static boolean isPalindrome(String s) {
-        var normalized = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-        var reversedWord = reversedWord(normalized);
-        return normalized.equals(reversedWord);
-    }
-
-    private static String reversedWord(String s) {
+        var str = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase().split("");
         var l = 0;
-        var r = s.length() - 1;
-        var str = s.split("");
-        while (l < r) {
-            var tmp = str[l];
-            str[l] = str[r];
-            str[r] = tmp;
+        var r = str.length - 1;
+        while(l < r) {
+            if(!str[l].equals(str[r])) {
+                return false;
+            }
             l++;
             r--;
         }
-        StringBuilder builder = new StringBuilder();
-        for (String w : str) {
-            builder.append(w);
-        }
-        return builder.toString();
+        return true;
     }
 }
