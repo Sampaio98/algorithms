6260a6aa4237955d1541e3b02f63de2796958069
allisson.sampaio <allisson.sampaio@agi.com.br>
2025-11-11 21:18:04 -0300

    algorithms

--- src/leetcode/WordPattern.java
+++ src/leetcode/WordPattern.java
@@ -5,22 +5,27 @@ import java.util.HashMap;
 public class WordPattern {
 
     public static void main(String[] args) {
-//        System.out.println(wordPattern("abba", "dog cat cat dog"));
-//        System.out.println(wordPattern("abba", "dog dog dog dog"));
+        System.out.println(wordPattern("abba", "dog cat cat dog"));
+        System.out.println(wordPattern("abba", "dog dog dog dog"));
         System.out.println(wordPattern("abba", "dog cat cat fish"));
     }
 
     public static boolean wordPattern(String pattern, String s) {
         String[] words = s.split(" ");
-        var map = new HashMap<String, Character>();
+        var map = new HashMap<Character, String>();
+        if(words.length != pattern.length()) return false;
         for (int i = 0; i < pattern.length(); i++) {
             var letter = pattern.charAt(i);
-            if (map.containsValue(letter)) {
-                if (!map.containsKey(words[i])) {
+            if (map.containsKey(letter)) {
+                if (!map.get(letter).equals(words[i])) {
                     return false;
                 }
             } else {
-                map.put(words[i], letter);
+                if (map.containsValue(words[i])) {
+                    return false;
+                } else {
+                    map.put(letter, words[i]);
+                }
             }
         }
         return true;
