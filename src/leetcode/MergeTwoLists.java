19fa8f7bf41a14e64402feded42ad7601c8bd5e0
Sampaio98 <allissonsampaio98@gmail.com>
2026-01-06 20:03:42 -0300

    Merge two lists leetcode problem

---
+++ src/leetcode/MergeTwoLists.java
@@ -0,0 +1,76 @@
+package leetcode;
+
+public class MergeTwoLists {
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+
+        ListNode secondNode1 = new ListNode(1);
+        ListNode secondNode2 = new ListNode(3);
+        ListNode secondNode3 = new ListNode(4);
+        secondNode1.next = secondNode2;
+        secondNode2.next = secondNode3;
+
+        System.out.println(mergeTwoLists(node1, secondNode1));
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        var ref = new ListNode();
+        var curr = ref;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                curr.next = list1;
+                curr = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                curr = list2;
+                list2 = list2.next;
+            }
+        }
+
+        curr.next = list1 != null ? list1 : list2;
+
+        return ref.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            var sb = new StringBuilder();
+            sb.append("[");
+            var node = this;
+            while (node != null) {
+                sb.append(node.val);
+                if (node.next != null) {
+                    sb.append(",");
+                }
+                node = node.next;
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+    }
+
+
+}
