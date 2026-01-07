499b31a5b2216cafdbc4932cea9683cd3788af1e
Sampaio98 <allissonsampaio98@gmail.com>
2026-01-06 21:30:55 -0300

    BubbleSort implementation

---
+++ src/implementation/sort/BubbleSort.java
@@ -0,0 +1,30 @@
+package implementation.sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        var bubbleSort = new BubbleSort();
+        bubbleSort.sort(new int[]{5, 4, 3, 2, 1});
+        bubbleSort.sort(new int[]{1, 2, 3, 4, 5});
+        bubbleSort.sort(new int[]{1, 2, 3, 5, 4});
+    }
+
+
+    public void sort(int[] nums) {
+        for (int x : nums) {
+            var isSorted = true;
+            System.out.println(Arrays.toString(nums));
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] > nums[i + 1]) {
+                    isSorted = false;
+                    int temp = nums[i];
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = temp;
+                }
+            }
+            if (isSorted) return;
+        }
+    }
+}
