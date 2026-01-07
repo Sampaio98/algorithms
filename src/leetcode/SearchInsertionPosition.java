e5f4fe229aa78faff060c584f10d59e04dfc1e57
Sampaio98 <allissonsampaio98@gmail.com>
2026-01-07 20:54:53 -0300

    Search Insertion Position leetcode problem

---
+++ src/leetcode/SearchInsertionPosition.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class SearchInsertionPosition {
+
+    void main(String[] args) {
+        IO.println(searchInsert(new int[]{1, 3, 5, 6}, 5));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (target < nums[mid]) {
+                r = mid - 1;
+            } else if (target > nums[mid]) {
+                l = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return l;
+    }
+}
