86bcc539695c3e710ea2559923c83eb03ffbc18d
Sampaio98 <allissonsampaio98@gmail.com>
2026-03-09 10:35:20 -0300

    number of bits

---
+++ src/leetcode/NumberOfBits.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class NumberOfBits {
+
+    void main() {
+        IO.println(hammingWeight(11));
+        IO.println(hammingWeight(128));
+        IO.println(hammingWeight(2147483645));
+    }
+
+    public int hammingWeight(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            if (((n >> i) & 1) == 1) {
+                res += 1;
+            }
+        }
+        return res;
+    }
+}
