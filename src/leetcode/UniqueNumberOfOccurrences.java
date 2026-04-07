4e05308466eee2a645553e0cd4656d51bb348662
Allisson <allissonsampaio98@gmail.com>
2026-04-06 21:50:28 -0300

    (Leetcode) Unique Number of Occurrences

--- src/leetcode/UniqueNumberOfOccurrences.java
+++ src/leetcode/UniqueNumberOfOccurrences.java
@@ -1,17 +1,23 @@
 package leetcode;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class UniqueNumberOfOccurrences {
 
     void main() {
-        
+        IO.println(uniqueOccurrences(new int[]{1, 2, 2, 1, 1, 3}));
+        // IO.println(uniqueOccurrences(new int[]{1, 2}));
+
     }
 
     public boolean uniqueOccurrences(int[] arr) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        return false;
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        Set<Integer> set = new HashSet<>(map.values());
+        return set.size() == map.size();
     }
-
 }
-
