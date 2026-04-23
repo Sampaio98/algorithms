d26e0e1bb0eea5fa0353d9e892bc9c4907b21132
Sampaio98 <allissonsampaio98@gmail.com>
2026-04-23 18:53:12 -0300

    Implemented HashMap

--- src/implementation/hashmap/HashMapImpl.java
+++ src/implementation/hashmap/HashMapImpl.java
@@ -1,66 +1,62 @@
 package implementation.hashmap;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class HashMapImpl<K, V> {
-    private static final int INITIAL_CAPACITY = 16;
+
+    private static final int INITIAL_CAPACITY = 10;
     private static final float LOAD_FACTOR = 0.75f;
 
     private Tuple<K, V>[] buckets;
     private int size;
 
     public HashMapImpl() {
-        this.size = 0;
         this.buckets = new Tuple[INITIAL_CAPACITY];
+        this.size = 0;
     }
 
     public void put(K key, V val) {
-        int index = hash(key);
+        var index = hash(key);
 
         if (buckets[index] == null) {
             buckets[index] = new Tuple<>(key, val);
         } else {
-            Tuple<K, V> curr = buckets[index];
-            Tuple<K, V> before = curr;
+            var curr = buckets[index];
+            var prev = curr;
             while (curr != null) {
-                if (curr.key.equals(key)) {
+                if (Objects.equals(curr.key, key)) {
                     curr.val = val;
                     return;
                 }
-                before = curr;
+                prev = curr;
                 curr = curr.next;
             }
-            before.next = new Tuple<>(key, val);
+            prev.next = new Tuple<>(key, val);
         }
         size++;
-        if (size >= buckets.length * LOAD_FACTOR) resize();
+        if (size >= buckets.length * LOAD_FACTOR) {
+            resize();
+        }
     }
 
     public V get(K key) {
-        int index = hash(key);
-        Tuple<K, V> bucket = buckets[index];
-        while (bucket != null) {
-            if (bucket.key.equals(key)) {
-                return bucket.val;
+        var index = hash(key);
+        var curr = buckets[index];
+        while (curr != null) {
+            if (Objects.equals(curr.key, key)) {
+                return curr.val;
             }
-            bucket = bucket.next;
+            curr = curr.next;
         }
         return null;
     }
 
-    //TODO
-    public V remove(K key) {
-        int index = hash(key);
-        return null;
-    }
-
-    public int size() {
-        return size;
-    }
-
     public boolean containsKey(K key) {
-        int index = hash(key);
+        var index = hash(key);
         var curr = buckets[index];
         while (curr != null) {
-            if (curr.key.equals(key)) {
+            if (Objects.equals(curr.key, key)) {
                 return true;
             }
             curr = curr.next;
@@ -68,27 +64,56 @@ public class HashMapImpl<K, V> {
         return false;
     }
 
+    public boolean containsValue(V val) {
+        for (Tuple<K, V> bucket : buckets) {
+            var curr = bucket;
+            while (curr != null) {
+                if (Objects.equals(curr.val, val)) {
+                    return true;
+                }
+                curr = curr.next;
+            }
+        }
+        return false;
+    }
+
+    //TODO
+    public V remove(K key) {
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void clear() {
+        Arrays.fill(buckets, null);
+        size = 0;
+    }
+
     private void resize() {
         var oldBuckets = buckets;
         this.buckets = new Tuple[oldBuckets.length * 2];
-        for (Tuple<K, V> bucket : oldBuckets) {
-            while (bucket != null) {
-                var next = bucket.next;
-                int index = hash(bucket.key);
-                bucket.next = buckets[index];
-                buckets[index] = bucket;
-
-                bucket = next;
+        this.size = 0;
+        for (Tuple<K, V> oldBucket : oldBuckets) {
+            var curr = oldBucket;
+            while (curr != null) {
+                put(curr.key, curr.val);
+                curr = curr.next;
             }
         }
     }
 
     private int hash(K key) {
-        if (key == null) return 0;
         return Math.floorMod(key.hashCode(), buckets.length);
     }
 
     static class Tuple<K, V> {
+
         K key;
         V val;
         Tuple<K, V> next;
